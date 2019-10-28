85626c1acc1f79cb29374fc71da459dce63ab8aa
medobog <44975400+medobog@users.noreply.github.com>
2019-10-28 10:48:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using Location.Infrastructure;
+
+namespace SignalR_ClientConsole
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var Connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:44343/Informacije")
+                .Build();
+
+            Connection.StartAsync().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            Connection.On<string>("ReciveInfo", obj => {
+
+                var deserializedObj = JsonConvert.DeserializeObject<InfoModel>(obj);
+                Console.WriteLine(deserializedObj.ToString());
+            });
+
+
+
+            Console.Read();
+            Connection.StopAsync().Wait();
+        }
+    }
+}
